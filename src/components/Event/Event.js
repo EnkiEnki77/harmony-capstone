56382cc950e3194cdd6824d9ae5405861d55d65a
Christopher Vallez <Topher@ChristophersMBP.lan>
2022-02-23 19:46:54 -0800

    event.js updated

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,12 +1,47 @@
-import React from 'react'
-import { EventContainer } from './Event.styles'
+import React from "react";
+import Button from "../Button/Button";
+import {
+  EventContainer,
+  EventItem,
+  ArtistImage,
+  Date,
+  Day,
+  Time,
+  DateTime,
+  ArtInfo,
+  Artist,
+  Location,
+  DownArrow,
+} from "./Event.styles";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
-const Event = () => {
+const Event = (props) => {
   return (
-    <EventContainer>
+    <>
+      <EventContainer>
+        <EventItem>
+          <DownArrow icon={faAngleDown} />
+          <ArtistImage src={props.artistImage} alt="Artist Image" />
+          <DateTime>
+            <Date>{props.date}</Date>
+            <Day>{props.day} • </Day>
+            <Time>{props.time} </Time>
+          </DateTime>
+          <ArtInfo>
+            <Artist>{props.artist}</Artist>
+            <Location>{props.location}</Location>
+          </ArtInfo>
+        </EventItem>
+        <Button
+          hover={true}
+          land={false}
+          text="See Tickets"
+          buttonAltW="134px"
+          buttonAltH="41px"
+        />
+      </EventContainer>
+    </>
+  );
+};
 
-    </EventContainer>
-  )
-}
-
-export default Event
+export default Event;
